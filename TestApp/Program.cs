aa026359676e63a3cff3c109e4eec9b26b2f0ee8
TimHaa <36132493+TimHaa@users.noreply.github.com>
2018-02-04 16:47:04 +0100

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,7 +9,7 @@ namespace TestApp
             Console.WriteLine("Hello World!");
             int a = 0;
             string b = "test";
-            float d = 3.2f;
+            double c = 3.3;
 
         }
     }
