1b7fac8868088199b9b442f55efd8137911bdc83
TimHaa <36132493+TimHaa@users.noreply.github.com>
2018-03-01 18:05:19 +0100

    Added MaximumUniqueString()

---
+++ TestApp/MaximumUniqueString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TestApp
+{
+    class MaximumUniqueString
+    {
+        //find the string with most unique characters from an array of strings
+        public MaximumUniqueString() { }
+        public void Find(string[] inputStrings)
+        {
+            int indexCount = 0;
+            int[] count = new int[inputStrings.Length];
+            foreach (string word in inputStrings)
+            {
+                bool[] doublet = new bool[word.Length];
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!doublet[i])
+                    {
+                        count[indexCount]++;
+                        for (int j = i + 1; j < word.Length; j++)
+                        {
+                            if(word[j] == word[i]) { doublet[j] = true; }
+                        }
+                    }
+                }
+                Console.Write(word + " ");
+                indexCount++;
+            }
+            Console.WriteLine();
+            string longest = inputStrings[Array.IndexOf(count, count.Max())];
+            foreach (int nr in count) { Console.Write(nr + "\t"); }
+            Console.WriteLine();
+            Console.WriteLine(longest + " with a unique-letter-count of " + count.Max());
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,14 +6,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            KthElement k = new KthElement();
-            int[] inputK = { 1, 3, 5 };
-            int[][] inputN = {
-                new int[] {1,3},
-                new int[] {6,9},
-                new int[] {10, 13},
-            };
-            k.Find(inputN, inputK);
+            
             Console.ReadLine();
 
         }
