1607ded11bc691a4be3f518f301bd38f89ed0fd7
TimHaa <36132493+TimHaa@users.noreply.github.com>
2018-03-01 12:38:34 +0100

    added 1x == x

--- TestApp/EquationSolver.cs
+++ TestApp/EquationSolver.cs
@@ -7,48 +7,41 @@ namespace TestApp
     class EquationSolver
     {
         public EquationSolver() { }
-
-        public void Linear(string equation){
+        public void Linear(string equation)
+        {
             //solve linear equation by summing up numbers and x's and dividing sumX/sumNr
+
             equation = equation.Replace(" ", string.Empty);
             Console.WriteLine(equation);
-            
             int xLeft = 0;
             int xRight = 0;
             int nrLeft = 0;
             int nrRight = 0;
-            
             bool sideChanged = false;
             int posPreviousSign = 0;
             
             for (int i = 0; i < equation.Length; i++)
             {
-                Console.WriteLine("****");
                 if (equation[i] == '=')
                 {
-
                     Evaluate(equation, posPreviousSign, i, sideChanged, ref xRight, ref xLeft, ref nrRight, ref nrLeft);
                     sideChanged = true;
-                    
                     posPreviousSign = i+1;
                 }
                 else if(equation[i]== '+')
                 {
                     Evaluate(equation, posPreviousSign, i, sideChanged, ref xRight, ref xLeft, ref nrRight, ref nrLeft);
-
                     posPreviousSign = i+1;
                 }
                 else if (equation[i] == '-')
                 {
                     Evaluate(equation, posPreviousSign, i, sideChanged, ref xRight, ref xLeft, ref nrRight, ref nrLeft);
-
                     posPreviousSign = i;
                 }
             }
-
             Evaluate(equation, posPreviousSign, equation.Length, sideChanged, ref xRight, ref xLeft, ref nrRight, ref nrLeft);
 
-            float nrSum = nrRight - nrLeft;//type float so solution performs float division, not int
+            float nrSum = nrRight - nrLeft;
             float xSum = xLeft - xRight;
             Console.WriteLine("sum x: {0};sum nr : {1}", xSum, nrSum);
             Console.WriteLine("left nr: {0};right nr : {1}", nrLeft, nrRight);
@@ -56,7 +49,7 @@ namespace TestApp
             Console.WriteLine();
             if (xSum == 0 && nrSum == 0)
             {
-                Random rand = new Random();//just fun
+                Random rand = new Random();
                 int rnd = rand.Next(1000);
                 Console.WriteLine("There are infinit possible solutions. May I propose {0}?", rnd);
             }
@@ -69,12 +62,13 @@ namespace TestApp
                 float solution = nrSum/xSum ;
                 Console.WriteLine("X equals {0}.", solution);
             }
-            
         }
+
         private void Evaluate(string fullString, int start, int end, bool side, ref int xRSum, ref int xLSum, ref int nrRSum, ref int nrLSum)
         {
             //Check if contains x and then add to according sum
-            string term = fullString.Substring(start, end-start); //SubString zweite zahl länge
+
+            string term = fullString.Substring(start, end-start);
             int value;
             if (term.Contains("x"))
             {
@@ -83,7 +77,6 @@ namespace TestApp
                 if (side) { xRSum += value; }
                 else { xLSum += value; }
                 Console.WriteLine("left x: {0};right x : {1}", xLSum, xRSum);
-
             }
             else
             {
